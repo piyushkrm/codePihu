3ecb3b299b7c4a0c5afe817717d4fa785e01c036
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-30 10:43:20 +0530

    Project 1 writing work is done

---
+++ Project1/helmet-detection.js
@@ -0,0 +1,6 @@
+// Function to navigate back
+function goBack() {
+    window.history.back("file:///F:/Projects/Project1/helmet-detection.html");
+}
+
+console.log("Helmet Detection System Page Loaded!");

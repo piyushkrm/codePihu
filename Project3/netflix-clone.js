db8ae8b333ba55e1d2a18f3b520289ccd23b6179
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-30 11:22:27 +0530

    Project 3written work done

---
+++ Project3/netflix-clone.js
@@ -0,0 +1,9 @@
+// Go back to the previous page
+function goBack() {
+    window.history.back("file:///F:/Projects/Project1/helmet-detection.html");
+}
+
+// Log a message
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Welcome to the Vehicle Speed Detection System Page!");
+});

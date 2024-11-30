7442bef88ec84f0d00b047248ce6e42e476dc341
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-30 15:45:01 +0530

    written work done for project13

---
+++ Project13/dance-website-project.js
@@ -0,0 +1,9 @@
+// Go back to the previous page
+function goBack() {
+    window.history.back("file:///F:/Projects/main/index.html");
+}
+
+// Log a message
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Start dancing...");
+});
